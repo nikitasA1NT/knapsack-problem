52894508b19e2cc71545bf0a0ce051adae2c2e20
nikitasnt <14nikita14m@gmail.com>
2023-01-09 22:06:17 +0400

    Knapsack problem solution

---
+++ KnapsackLib/Knapsack.cs
@@ -0,0 +1,58 @@
+﻿namespace KnapsackLib;
+
+public record Item(string Name, int Value, int Weight);
+
+public static class KnapsackProblem
+{
+    // Method solve knapsack problem
+    public static List<Item> Solve(List<Item> items, int capacity)
+    {
+        // Create matrix
+        var matrix = new int[items.Count + 1, capacity + 1];
+
+        // Fill matrix
+        int i;
+        int j;
+        for (i = 1; i <= items.Count; i++)
+        {
+            for (j = 1; j <= capacity; j++)
+            {
+                if (items[i - 1].Weight > j)
+                {
+                    matrix[i, j] = matrix[i - 1, j];
+                }
+                else
+                {
+                    matrix[i, j] = Math.Max(matrix[i - 1, j], matrix[i - 1, j - items[i - 1].Weight] + items[i - 1].Value);
+                }
+            }
+        }
+
+        // Find items
+        var result = new List<Item>();
+        i = items.Count;
+        j = capacity;
+        while (i > 0 && j > 0)
+        {
+            if (matrix[i, j] != matrix[i - 1, j])
+            {
+                result.Add(items[i - 1]);
+                j -= items[i - 1].Weight;
+            }
+
+            i--;
+        }
+
+        return result;
+    }
+    
+    public static int SummaryValue(this List<Item> items)
+    {
+        return items.Sum(_ => _.Value);
+    }
+    
+    public static int SummaryWeight(this List<Item> items)
+    {
+        return items.Sum(_ => _.Weight);
+    }
+}
--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using KnapsackLib;
+
+var itemList = new List<Item>
+{
+    new("Gold ingot", 100, 5),
+    new("Piece of sausage in dough", 20, 2),
+    new("Shawarma", 50, 4)
+};
+
+const int knapsackCapacity = 10;
+
+Console.WriteLine($"Knapsack capacity: {knapsackCapacity}");
+
+var solvedList = KnapsackProblem.Solve(itemList, knapsackCapacity);
+
+Console.WriteLine($"Solved list:\n{string.Join(", ", solvedList.Select(_ => _.Name))}");
+Console.WriteLine($"Summary value: {solvedList.SummaryValue()}");
+Console.WriteLine($"Summary weight: {solvedList.SummaryWeight()}");
